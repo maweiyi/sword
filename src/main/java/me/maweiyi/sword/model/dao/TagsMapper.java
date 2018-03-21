a709a8fff344b2b9db461f98fd4472464903ee8a
maweiyi <ma823285807@gmail.com>
2018-03-21 20:57:38 +0800

    add mapper

---
+++ src/main/java/me/maweiyi/sword/model/dao/TagsMapper.java
@@ -0,0 +1,12 @@
+package me.maweiyi.sword.model.dao;
+
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author MWY
+ * @date 3/21/18
+ */
+
+@Repository
+public interface TagsMapper {
+}
---
+++ src/main/java/me/maweiyi/sword/model/dao/UsersMapper.java
@@ -0,0 +1,11 @@
+package me.maweiyi.sword.model.dao;
+
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author MWY
+ * @date 3/21/18
+ */
+@Repository
+public interface UsersMapper {
+}
